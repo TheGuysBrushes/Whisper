639ab4e47ae4e75bcd442a30f039cb08a057d074
flo5324 <flo.noel@hotmail.fr>
2017-01-18 16:21:05 +0100

    Préparation génération clé privée

--- src/main/java/Encryption/KeyGenerator.java
+++ src/main/java/Encryption/KeyGenerator.java
@@ -5,16 +5,22 @@
  */
 package Encryption;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.math.BigInteger;
 import java.util.Random;
 
 /**
- *
  * @author flodavid
  */
 public class KeyGenerator {
-    BigInteger m;
-    BigInteger n;
+    private final static Logger logger = LoggerFactory.getLogger(KeyGenerator.class);
+
+
+    private BigInteger e;
+    private BigInteger m;
+    private BigInteger n;
 
     public BigInteger generateKey() {
         Random rand = new Random();
@@ -23,12 +29,47 @@ public class KeyGenerator {
 
         // n definition
         n = p.multiply(q);
-        
+
         // m definition
         BigInteger p_less = p.subtract(new BigInteger("1") );
         m = p_less.multiply(q.subtract(new BigInteger("1") ));
-        
+
         return n;
     }
 
+    public BigInteger generatePrivateKey() {
+        logger.info("Debut méthode");
+        BigInteger n = new BigInteger("12");
+        BigInteger u = new BigInteger("12");
+        BigInteger r = new BigInteger("12");
+
+        BigInteger pgcd = e.gcd(m);
+        logger.debug("Valeur du pgcd({},{}) : {}",e,m,pgcd);
+
+        return u;
+    }
+
+    public BigInteger getE() {
+        return e;
+    }
+
+    public void setE(BigInteger e) {
+        this.e = e;
+    }
+
+    public BigInteger getM() {
+        return m;
+    }
+
+    public void setM(BigInteger m) {
+        this.m = m;
+    }
+
+    public BigInteger getN() {
+        return n;
+    }
+
+    public void setN(BigInteger n) {
+        this.n = n;
+    }
 }
