e99840dcca22430d967b2b2c92e1b23cfcae9385
Florentin <flo.noel@hotmail.fr>
2017-01-19 17:56:07 +0100

    Génération clé privée

--- src/main/java/Encryption/KeyGenerator.java
+++ src/main/java/Encryption/KeyGenerator.java
@@ -23,12 +23,16 @@ public class KeyGenerator {
     private BigInteger e;
     private BigInteger m;
     private BigInteger n;
+    private BigInteger u;
 
     /**
      * Generate a public key
+     *
      * @return a generated public key
      */
     public PublicKey generatePublicKey() {
+        logger.info("Génération clés publiques");
+
         Random rand = new Random();
 
         // p and q definition
@@ -39,32 +43,78 @@ public class KeyGenerator {
         n = p.multiply(q);
 
         // m definition
-        BigInteger p_less = p.subtract(new BigInteger("1") );
-        m = p_less.multiply(q.subtract(new BigInteger("1") ));
+        BigInteger p_less = p.subtract(new BigInteger("1"));
+        m = p_less.multiply(q.subtract(new BigInteger("1")));
 
         // e (public exponent) definition
-        e= new BigInteger("2");
+        e = new BigInteger("2");
 
-        while ( m.gcd(e) != new BigInteger("1") ) {
-            e.nextProbablePrime();
+        while ((m.gcd(e)).compareTo(new BigInteger("1")) != 0) {
+            e = e.nextProbablePrime();
         }
 
         // Create the key from calculated values
-        PublicKey key= new PublicKey(n, e);
+        PublicKey key = new PublicKey(n, e);
 
         return key;
     }
 
-    public BigInteger generatePrivateKey() {
-        logger.info("Debut méthode");
-        BigInteger n = new BigInteger("12");
-        BigInteger u = new BigInteger("12");
-        BigInteger r = new BigInteger("12");
+    public PrivateKey generatePrivateKey() {
+        logger.info("Génération clés privées");
+        BigInteger r_prec = e;
+        BigInteger u_prec = new BigInteger("1");
+        BigInteger v_prec = new BigInteger("0");
+        BigInteger r = m;
+        BigInteger u = new BigInteger("0");
+        BigInteger v = new BigInteger("1");
+        BigInteger r_next;
+        BigInteger u_next;
+        BigInteger v_next;
 
-        BigInteger pgcd = e.gcd(m);
-        logger.debug("Valeur du pgcd("+e+","+m+") : "+pgcd);
+        logger.debug("Lancement avec paramètres : ");
+        logger.debug("r_prec = " + r_prec + "\tr = " + r);
+        logger.debug("u_prec = " + u_prec + "\tu = " + u);
+        logger.debug("v_prec = " + v_prec + "\tv = " + v + "\n");
 
-        return u;
+        do {
+            r_next = r_prec.subtract((r_prec.divide(r)).multiply(r));
+            u_next = u_prec.subtract((r_prec.divide(r)).multiply(u));
+            v_next = v_prec.subtract((r_prec.divide(r)).multiply(v));
+
+            r_prec = r;
+            r = r_next;
+            u_prec = u;
+            u = u_next;
+            v_prec = v;
+            v = v_next;
+
+            if (u_next.compareTo(new BigInteger("0")) == -1) {
+                logger.debug("Valeur de u négative (u = " + u_next + ")");
+                u_next = solveNegativeValue(u_next);
+            }
+
+            logger.debug("r = " + r_next);
+            logger.debug("u = " + u_next);
+            logger.debug("v = " + v_next + "\n");
+        } while (r_next.compareTo(new BigInteger("0")) != 0);
+
+        this.u = u;
+
+        // Create the key from calculated values
+        PrivateKey key = new PrivateKey(n, u);
+        return key;
+    }
+
+    private BigInteger solveNegativeValue(BigInteger u) {
+        BigInteger k = new BigInteger("-1");
+        BigInteger u_new = u;
+
+        do {
+            u_new = u.subtract(k.multiply(m));
+            k = k.subtract(new BigInteger("1"));
+        } while (new BigInteger("2").compareTo(u_new) != -1 || u_new.compareTo(m) != -1);
+
+        return u_new;
     }
 
     public BigInteger getE() {
@@ -89,5 +139,13 @@ public class KeyGenerator {
 
     public void setN(BigInteger n) {
         this.n = n;
+    }
+
+    public BigInteger getU() {
+        return u;
+    }
+
+    public void setU(BigInteger u) {
+        this.u = u;
     }
 }
